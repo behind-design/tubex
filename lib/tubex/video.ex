a9041a3f1863aef9f058ef2c113c1c01e63e40ab
Bernat Jufré <info@behind.design>
2017-08-21 13:50:12 +0200

    Added options \ [] to Video.detail/2 function

--- lib/tubex/video.ex
+++ lib/tubex/video.ex
@@ -5,8 +5,8 @@ defmodule Tubex.Video do
   @doc """
   fetch contents details
   """
-  def detail(video_id) do
-    opts = [key: Tubex.api_key, id: video_id, part: "contentDetails"]
+  def detail(video_id, options \\ []) do
+    opts = Keyword.merge([key: Tubex.api_key, id: video_id, part: "contentDetails"], options)
     case Tubex.API.get(Tubex.endpoint <> "/videos", opts) do
       {:ok, response} ->
         response
